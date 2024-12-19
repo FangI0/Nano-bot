c4e4d38d50feb2745fbef7222db0892a248870e3
FangI0 <mohamedking170170170@gmail.com>
2024-12-20 01:02:13 +0200

    Update تغير-حقوق.js

--- plugins/تغير-حقوق.js
+++ plugins/تغير-حقوق.js
@@ -19,5 +19,5 @@ else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/no
 }}
 handler.help = ['wm']
 handler.tags = ['sticker']
-handler.command = /^تغير-حقوق$/i
+handler.command = /^سرق$/i
 export default handler
